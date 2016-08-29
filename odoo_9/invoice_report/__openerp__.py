b81df6acd78d8d1a7e7fa887e3e6e73d90c3c993
riya <riya.sathvara@gmail.com>
2016-08-29 18:56:00 +0530

    Report inherting

---
+++ odoo_9/invoice_report/__openerp__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Invoice Report',
+    'summary' : '',
+    'version': '1.0',
+    'category': 'Sale',
+    'website': '',
+    'complexity': "normal",
+    'sequence': 20,
+    'description': """
+
+""",
+
+    # Dependencies
+    'depends': ['sale'],
+    'external_dependencies': {},
+
+    'data': [
+        'report/report.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'active':True,
+    'auto_install': False,
+}
